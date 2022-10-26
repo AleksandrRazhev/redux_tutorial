157d2535be08fd1a0c608d9d55eb89d5183c1032
AleksandrRazhev <alexandrrazh@gmail.com>
2022-10-26 16:05:34 +0300

    refactoring of styles has been performed

--- redux-ts-tutorial/src/store/reducers/userReducer.ts
+++ redux-ts-tutorial/src/store/reducers/userReducer.ts
@@ -1,39 +1,10 @@
-enum UserActionTypes {
-  FETCH_USERS = "FETCH_USERS",
-  FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS",
-  FETCH_USERS_ERROR = "FETCH_USERS_ERROR",
-}
-
-interface UserState {
-  users: any[];
-  loading: boolean;
-  error: null | string;
-}
-
-interface FetchUserAction {
-  type: UserActionTypes.FETCH_USERS;
-}
-
-interface FetchUserSuccessAction {
-  type: UserActionTypes.FETCH_USERS_SUCCESS;
-  payload: any[];
-}
-
-interface FetchUserErrorAction {
-  type: UserActionTypes.FETCH_USERS_ERROR;
-  payload: string;
-}
+import { UserActionTypes, UserState, UserAction } from "../../types/user";
 
 const initialState: UserState = {
   users: [],
   loading: false,
   error: null,
 };
-
-type UserAction =
-  | FetchUserAction
-  | FetchUserSuccessAction
-  | FetchUserErrorAction;
 
 export const userReducer = (
   state = initialState,
