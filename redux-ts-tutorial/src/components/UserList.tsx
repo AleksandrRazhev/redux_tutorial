50e8342d7d8652a1cb59d26fd2ff6da0527f8523
AleksandrRazhev <alexandrrazh@gmail.com>
2022-10-27 14:27:16 +0300

    todo types created

--- redux-ts-tutorial/src/components/UserList.tsx
+++ redux-ts-tutorial/src/components/UserList.tsx
@@ -1,9 +1,32 @@
-import React from "react";
-import { userTypedSelector } from "../hooks/userTypedSelector";
+import React, { useEffect } from "react";
+import { useActions } from "../hooks/useActions";
+
+import { useTypedSelector } from "../hooks/useTypedSelector";
 
 const UserList: React.FC = () => {
-  const { users, error, loading } = userTypedSelector((state) => state.user);
-  return <div></div>;
+  const { users, error, loading } = useTypedSelector((state) => state.user);
+
+  const { fetchUsers } = useActions();
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) {
+    return <p>...идет загрузка</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <div>
+      {users.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default UserList;
---
+++ redux-ts-tutorial/src/store/reducers/todoReducer.ts
@@ -0,0 +1,6 @@
+import { TodoState } from "../../types/todo";
+
+export const todoReducer = (state: TodoState, action) => {
+  switch (action.type) {
+  }
+};
